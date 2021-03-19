d191d6f8bee9946b49397965cbdeba50186d568f
Meia Natividad <meianatividad@gmail.com>
2021-03-19 10:38:58 -0700

    connect react frontend w express backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+
+// App.propTypes = {
+//   children: React.PropTypes.node,
+// };
 
 function App() {
+  const [apiResponse, setAPIResponse] = React.useState('');
+
+  function callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then((res) => res.text())
+      .then((res) => setAPIResponse(res))
+      .catch((err) => err);
+  }
+
+  useEffect(() => {
+    callAPI();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="App-title">
+          {' '}
+          Welcome to React{' '}
+        </h1>{' '}
+      </header>{' '}
+      <p className="App-intro"> {apiResponse} </p>
     </div>
   );
 }
